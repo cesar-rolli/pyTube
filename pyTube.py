ee6462f56d12acb07866cfdc864abfa6e632c207
Cesar Rolli <52143802+cesar-rolli@users.noreply.github.com>
2023-04-09 21:58:03 -0300

    changing moviepy to ffmpeg

--- pyTube.py
+++ pyTube.py
@@ -9,6 +9,9 @@ import shutil
 
 from moviepy.editor import *
 
+
+import time
+
 resolutionList = [
   "144p", 
   "240p", 
@@ -20,7 +23,7 @@ resolutionList = [
   "2160p", 
   "4320p"]
 
-#Layout
+# Layout
 def Widgets(): 
   #row 1: input label
   link_label = Label(
@@ -138,34 +141,34 @@ def Widgets():
 
 
 
-#Browse file 
+# Browse file 
 def Browse(): 
   download_Directory = filedialog.askdirectory(initialdir = "YOUR DIRECTORY PATH") 
   download_Path.set(download_Directory) 
 
 
 
-#Download video
+# Download video
 def Download():
-  #Create a folder for temporary archives
+  # Create a folder for temporary archives
   os.chdir(download_Path.get())
-  os.mkdir("Temporary Folder from PyTube")
+  os.mkdir("temp")
 
 
-  #Responsible to initializate download, getting URL and path
+  # Responsible to initializate download, getting URL and path
   Youtube_link = video_Link.get() 
-  download_Folder = download_Path.get() + "/Temporary Folder from PyTube"
+  download_Folder = download_Path.get() + "/temp"
   getVideo = YouTube(Youtube_link) 
 
 
-  #Download video that will be used as audio
+  # Download video that will be used as audio
   videoStream = getVideo.streams.filter(
     res = "360p", 
     file_extension = "mp4").first()
   videoStream.download(download_Folder) 
   
 
-  #Rename video that will be used as audio
+  # Rename video that will be used as audio
   videoTitle = videoStream.title
   removable = '|'
 
@@ -173,34 +176,30 @@ def Download():
     if letra in videoTitle:
       videoTitle = videoTitle.replace(letra, '')
 
-  os.chdir(download_Path.get() + "/Temporary Folder from PyTube")
+  os.chdir(download_Path.get() + "/temp")
   os.rename(f'{videoTitle}' + ".mp4", "audioFile.mp4")
 
 
-  #Download video in preset quality
+  # Download video in preset quality
   videoResolution = setResolution.get()
   videoStream = getVideo.streams.filter(
     only_video = True, 
     res = videoResolution, 
     file_extension = "mp4").first()
   videoStream.download(download_Folder) 
+
+  os.chdir(download_Path.get() + "/temp")
+  os.rename(f'{videoTitle}' + ".mp4", "video.mp4")
+
+  # Join the audio to video
+  os.system("ffmpeg -i audioFile.mp4 -codec:a libmp3lame audio.mp3")
+  os.system("ffmpeg -hwaccel_output_format cuda -y -i video.mp4 -i audio.mp3 -c:v copy -c:a aac output.mp4")
+
+  # Move file to path preset
+  os.rename(download_Path.get() + "/temp/output.mp4", download_Path.get() + f'/{videoTitle}')
   
-
-  #Join the audio to video 
-  clip = VideoFileClip(f'{videoTitle}' + ".mp4")
-  audioclip = AudioFileClip("audioFile.mp4")
-  videoclip = clip.set_audio(audioclip)
-
-  os.chdir(download_Path.get())
-  videoclip.write_videofile(f'{videoTitle}' + ".mp4")
-
-
-  #Remove temporary folder
-  shutil.rmtree("Temporary Folder from PyTube")
-
-
-  #Display a message informing download has been completed
-  messagebox.showinfo("SUCCESSFULLY", "DOWNLOADED AND SAVED IN\n" + download_Folder) 
+  # Remove temporary folder
+  shutil.rmtree(download_Path.get() + "/temp")
 
 root = tk.Tk() 
 
@@ -218,4 +217,4 @@ setResolution = StringVar()
    
 Widgets() 
    
-root.mainloop() 
+root.mainloop()
